e5f801c90316fdda484b93fe71236d7dc0752b2e
kirby10000 <l603109@sgdc.nl>
2025-06-24 13:52:36 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 app.use(express.static("public"));
 
-const users = {}; // socket.id => { name, color }
+const users = {}; // socket.id => { name, color, rooms: Set }
 const pastelColors = [
   "#AEE4FF", "#BDF5D7", "#FFF5B7", "#FFD6C4", "#E6D6FF", "#FFC9DE"
 ];
@@ -13,23 +13,38 @@ function getRandomColor() {
   return pastelColors[Math.floor(Math.random() * pastelColors.length)];
 }
 
+// Hulpfunctie om privé-kamernaam te maken (alfabetisch)
+function makePrivateRoomName(u1, u2) {
+  return [u1, u2].sort().join("_");
+}
+
 io.on("connection", (socket) => {
   console.log("Verbonden:", socket.id);
 
   socket.on("register", (name) => {
-    users[socket.id] = { name, color: getRandomColor() };
-    io.emit("all users", Object.values(users));
+    users[socket.id] = { name, color: getRandomColor(), rooms: new Set() };
+    io.emit("all users", Object.values(users).map(u => ({ name: u.name, color: u.color })));
   });
 
   socket.on("join room", (room) => {
+    if (!users[socket.id]) return;
     socket.join(room);
+    users[socket.id].rooms.add(room);
     console.log(`${users[socket.id].name} heeft ${room} betreden`);
+  });
+
+  socket.on("leave room", (room) => {
+    if (!users[socket.id]) return;
+    socket.leave(room);
+    users[socket.id].rooms.delete(room);
+    console.log(`${users[socket.id].name} heeft ${room} verlaten`);
   });
 
   socket.on("chat message", ({ tab, text }) => {
     const user = users[socket.id];
     if (user && text.trim()) {
-      io.emit("chat message", {
+      // Stuur alleen naar die room
+      io.to(tab).emit("chat message", {
         tab,
         user: user.name,
         text,
@@ -38,13 +53,36 @@ io.on("connection", (socket) => {
     }
   });
 
+  // Maak een privé-chat tussen 2 gebruikers
+  socket.on("start private chat", (otherUsername) => {
+    const user = users[socket.id];
+    if (!user) return;
+
+    // Vind socket.id van andere gebruiker
+    const otherSocketId = Object.keys(users).find(id => users[id].name === otherUsername);
+    if (!otherSocketId) return;
+
+    const roomName = makePrivateRoomName(user.name, otherUsername);
+
+    // Join beide sockets in die room
+    socket.join(roomName);
+    users[socket.id].rooms.add(roomName);
+
+    io.sockets.sockets.get(otherSocketId)?.join(roomName);
+    users[otherSocketId].rooms.add(roomName);
+
+    // Stuur naar deze client en andere dat ze de privé room hebben
+    socket.emit("private chat started", roomName, otherUsername);
+    io.to(otherSocketId).emit("private chat started", roomName, user.name);
+  });
+
   socket.on("get users", () => {
-    socket.emit("all users", Object.values(users));
+    socket.emit("all users", Object.values(users).map(u => ({ name: u.name, color: u.color })));
   });
 
   socket.on("disconnect", () => {
     delete users[socket.id];
-    io.emit("all users", Object.values(users));
+    io.emit("all users", Object.values(users).map(u => ({ name: u.name, color: u.color })));
   });
 });
 
