d87561c512541f953d53ea58c2ff4237c35c758b
kirby10000 <l603109@sgdc.nl>
2025-06-24 12:01:11 +0200

    server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// server.js
 const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
@@ -6,15 +5,35 @@ const io = require("socket.io")(http);
 
 app.use(express.static("public"));
 
+const users = {};
+
+function randomColor() {
+  const colors = ["#ff6666", "#66ccff", "#66ff99", "#ffcc66", "#cc99ff", "#ff99cc"];
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
 io.on("connection", (socket) => {
-  console.log("Een gebruiker is verbonden.");
+  console.log("Nieuwe gebruiker verbonden:", socket.id);
+
+  socket.on("join", (name) => {
+    users[socket.id] = { name, color: randomColor() };
+    io.emit("update users", Object.values(users));
+  });
 
   socket.on("chat message", (msg) => {
-    io.emit("chat message", msg); // Stuur naar iedereen
+    const user = users[socket.id];
+    if (user) {
+      io.emit("chat message", {
+        user: user.name,
+        color: user.color,
+        text: msg
+      });
+    }
   });
 
   socket.on("disconnect", () => {
-    console.log("Een gebruiker is weg.");
+    delete users[socket.id];
+    io.emit("update users", Object.values(users));
   });
 });
 
